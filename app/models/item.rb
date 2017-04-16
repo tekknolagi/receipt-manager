369e415a708fcfcb32bf9d03ea69fd68897f337c
Maxwell Bernstein <tekknolagi@gmail.com>
2017-04-16 23:57:42 +0200

    Some more validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :receipt, optional: true
 
+  validates :product_id, numericality: { only_integer: true }
   validates :name, presence: true
-  validates :unit_price, presence: true
+  validates :unit_price, presence: true, numericality: true
+
+  def name=(n)
+    self[:name] = n.upcase
+  end
 end
